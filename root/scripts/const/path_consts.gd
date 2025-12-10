50d89826de7d3abf5b879c6b465b40586ea94530
Coldchise <119110121+Coldchise@users.noreply.github.com>
2025-12-10 16:07:52 +0800

    Main Menu
    Implemented Main Menu

---
+++ root/scripts/const/path_consts.gd
@@ -0,0 +1,11 @@
+class_name PathConsts
+## Original File MIT License Copyright (c) 2025 Coldchise
+
+const USER: String = "user://"
+
+const ROOT: String = "root/"
+const RES: String = "res://" + ROOT
+
+const RESOURCES: String = RES + "resources/"
+const ASSETS: String = RES + "assets/"
+const SFX: String = ASSETS + "audio/sfx/"
