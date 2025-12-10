d9ba63a5bfda9b8c0010d48f272e991c5cd521aa
Coldchise <119110121+Coldchise@users.noreply.github.com>
2025-12-10 16:49:41 +0800

    Bootsplash
    Modified

--- root/scenes/scene/boot_splash_scene/boot_splash_scene.gd
+++ root/scenes/scene/boot_splash_scene/boot_splash_scene.gd
@@ -6,11 +6,8 @@ extends Control
 @export var scene_manager_options_id: String = "fade_boot"
 
 var _boot_splash_color: Color = ProjectSettings.get("application/boot_splash/bg_color")
-var _boot_splash_image_path: String = ProjectSettings.get("application/boot_splash/image")
-var _boot_splash_texture: Texture = load(_boot_splash_image_path)
 
 @onready var boot_splash_color_rect: ColorRect = %BootSplashColorRect
-@onready var boot_splash_texture_rect: TextureRect = %BootSplashTextureRect
 
 func _ready() -> void:
 	_set_boot_splash()
@@ -18,9 +15,7 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 
-	# This wants an enum value, not a String
 	SceneManagerWrapper.change_scene(scene, scene_manager_options_id)
 
 func _set_boot_splash() -> void:
 	boot_splash_color_rect.color = _boot_splash_color
-	boot_splash_texture_rect.texture = _boot_splash_texture
